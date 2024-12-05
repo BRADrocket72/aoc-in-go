f56d10968c7506101d75381bad6a44f8ea2cd62d
bradley <bradleyvaal@gmail.com>
2024-12-04 19:18:58 -0600

    day 4 blantantly copied from github to learn about grids in go

--- 2024/04/code.go
+++ 2024/04/code.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	"strconv"
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
@@ -17,8 +22,163 @@ func main() {
 func run(part2 bool, input string) any {
 	// when you're ready to do part 2, remove this "not implemented" block
 	if part2 {
-		return "not implemented"
+		grid := map[image.Point]rune{}
+		for y, s := range strings.Split(strings.TrimSpace(string(input)), "\n") {
+			for x, r := range s {
+				grid[image.Point{x, y}] = r
+			}
+		}
+
+		adj := func(p image.Point, l int) []string {
+			delta := []image.Point{
+				{0, -1}, {1, 0}, {0, 1}, {-1, 0},
+				{-1, -1}, {1, -1}, {1, 1}, {-1, 1},
+			}
+
+			words := make([]string, len(delta))
+			for i, d := range delta {
+				for n := range l {
+					words[i] += string(grid[p.Add(d.Mul(n))])
+				}
+			}
+			return words
+		}
+
+		part2 := 0
+		for p := range grid {
+			part2 += strings.Count("AMAMASASAMAMAS", strings.Join(adj(p, 2)[4:], ""))
+		}
+		return part2
 	}
 	// solve part 1 here
-	return 42
+	grid := map[image.Point]rune{}
+	for y, s := range strings.Split(strings.TrimSpace(string(input)), "\n") {
+		for x, r := range s {
+			grid[image.Point{x, y}] = r
+		}
+	}
+
+	adj := func(p image.Point, l int) []string {
+		delta := []image.Point{
+			{0, -1}, {1, 0}, {0, 1}, {-1, 0},
+			{-1, -1}, {1, -1}, {1, 1}, {-1, 1},
+		}
+
+		words := make([]string, len(delta))
+		for i, d := range delta {
+			for n := range l {
+				words[i] += string(grid[p.Add(d.Mul(n))])
+			}
+		}
+		return words
+	}
+
+	part1 := 0
+	for p := range grid {
+		part1 += strings.Count(strings.Join(adj(p, 4), " "), "XMAS")
+	}
+	return part1
 }
+
+func testForward(lines []string) int {
+	sum := 0
+	for lineIndex, line := range lines {
+		for charIndex := range line {
+			if charIndex+4 < len(line) {
+				testString := line[charIndex : charIndex+4]
+				if testString == "XMAS" || testString == "SAMX" {
+					sum++
+					if line[charIndex] == 'X' {
+						fmt.Printf("(%s,%s)", strconv.Itoa(lineIndex), strconv.Itoa(charIndex))
+					}
+				}
+			}
+		}
+	}
+	return sum
+}
+
+func testDownwardLines(lines []string) int {
+	sum := 0
+	for lineIndex, line := range lines {
+		for charIndex := range line {
+			if lineIndex+3 < len(line) {
+				testString := string(lines[lineIndex][charIndex]) + string(lines[lineIndex+1][charIndex]) + string(lines[lineIndex+2][charIndex]) + string(lines[lineIndex+3][charIndex])
+
+				if testString == "SAMX" || testString == "XMAS" {
+					sum++
+					if lines[lineIndex][charIndex] == 'X' {
+						fmt.Printf("(%s,%s)", strconv.Itoa(lineIndex), strconv.Itoa(charIndex))
+					}
+				}
+			}
+		}
+	}
+	return sum
+
+}
+
+func testDiaganolDown(lines []string) int {
+	sum := 0
+	for lineIndex, line := range lines {
+		for charIndex := range line {
+			if lineIndex+3 < len(line) && charIndex+3 < len(line) {
+				testString := string(lines[lineIndex][charIndex]) + string(lines[lineIndex+1][charIndex+1]) + string(lines[lineIndex+2][charIndex+2]) + string(lines[lineIndex+3][charIndex+3])
+
+				if testString == "SAMX" || testString == "XMAS" {
+					sum++
+					if lines[lineIndex][charIndex] == 'X' {
+						fmt.Printf("(%s,%s)", strconv.Itoa(lineIndex), strconv.Itoa(charIndex))
+					}
+				}
+			}
+		}
+	}
+	return sum
+}
+
+func testDiaganolUp(lines []string) int {
+	sum := 0
+
+	for lineIndex, line := range lines {
+		for charIndex := range line {
+			if lineIndex-4 > -2 && charIndex+3 < len(line) {
+				testString := string(lines[lineIndex][charIndex]) + string(lines[lineIndex-1][charIndex+1]) + string(lines[lineIndex-2][charIndex+2]) + string(lines[lineIndex-3][charIndex+3])
+
+				if testString == "SAMX" || testString == "XMAS" {
+					sum++
+					if lines[lineIndex][charIndex] == 'X' {
+						fmt.Printf("(%s,%s)", strconv.Itoa(lineIndex), strconv.Itoa(charIndex))
+					}
+
+				}
+			}
+		}
+	}
+	return sum
+}
+
+// func testDiaganolUp(cols [][]string, lines []string) int {
+// 	sum := 0
+// 	for lineIndex, col := range cols {
+// 		for charIndex := range col {
+// 			if charIndex+4 < len(col) && lineIndex+4 < len(lines) {
+// 				testString := cols[lineIndex][charIndex] + cols[lineIndex+1][charIndex+1] + cols[lineIndex+2][charIndex+2] + cols[lineIndex+3][charIndex+3]
+// 				if testString == "XMAS" {
+// 					fmt.Print(lineIndex)
+// 					fmt.Print(cols[lineIndex])
+
+// 					sum++
+// 				}
+// 				if testString == "SAMX" {
+// 					fmt.Print(lineIndex)
+// 					fmt.Print(cols[lineIndex])
+
+// 					sum++
+// 				}
+// 			}
+// 		}
+
+// 	}
+// 	return sum
+// }
