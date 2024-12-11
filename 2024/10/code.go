b5651d07b9a7388e851e73d9c2e683da590a0c1a
bradley <bradleyvaal@gmail.com>
2024-12-10 21:05:45 -0600

    10288 is too high

--- 2024/10/code.go
+++ 2024/10/code.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	"strconv"
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
@@ -19,6 +24,143 @@ func run(part2 bool, input string) any {
 	if part2 {
 		return "not implemented"
 	}
+	ret := 0
+	trail_heads := make([]image.Point, 0)
+	x_max := 0
+	y_max := 0
+	grid := map[image.Point]int{}
+	for y, s := range strings.Split(strings.TrimSpace(string(input)), "\n") {
+		y_max = y_max + 1
+		for x, r := range s {
+			x_max = x_max + 1
+			string1 := string(r)
+			r_int, _ := strconv.Atoi(string1)
+			grid[image.Point{x, y}] = r_int
+
+			if r_int == 0 {
+				trail_heads = append(trail_heads, image.Point{x, y})
+			}
+
+		}
+	}
+
+	for _, trail_head := range trail_heads {
+		peaks := make([]image.Point, 0)
+		accessed_points := make([]image.Point, 0)
+		points_to_traverse := make([]image.Point, 0)
+
+		points_to_traverse = append(points_to_traverse, trail_head)
+		fmt.Print("new trail head")
+
+		for len(points_to_traverse) > 0 {
+			current_coords := points_to_traverse[0]
+
+			currentPoint := grid[points_to_traverse[0]]
+			if current_coords.X > 1 {
+				test_coords := image.Point{current_coords.X - 1, current_coords.Y}
+				test_point := grid[test_coords]
+				if test_point == 0 {
+					peaks = append(peaks, test_coords)
+				}
+				if currentPoint-test_point == 1 || currentPoint-test_point == -1 {
+					is_found := false
+					for _, travelled_Point := range accessed_points {
+						if travelled_Point.X == test_coords.X && travelled_Point.Y == test_coords.Y {
+							is_found = true
+						}
+					}
+					for _, travelled_Point := range points_to_traverse {
+						if travelled_Point.X == test_coords.X && travelled_Point.Y == test_coords.Y {
+							is_found = true
+						}
+					}
+					if !is_found {
+						points_to_traverse = append(points_to_traverse, test_coords)
+					}
+				}
+				if current_coords.X < x_max {
+					test_coords := image.Point{current_coords.X + 1, current_coords.Y}
+					test_point := grid[test_coords]
+					if test_point == 0 {
+						peaks = append(peaks, test_coords)
+					}
+					if currentPoint-test_point == 1 || currentPoint-test_point == -1 {
+						is_found := false
+						for _, travelled_Point := range accessed_points {
+							if travelled_Point.X == test_coords.X && travelled_Point.Y == test_coords.Y {
+								is_found = true
+							}
+						}
+						for _, travelled_Point := range points_to_traverse {
+							if travelled_Point.X == test_coords.X && travelled_Point.Y == test_coords.Y {
+								is_found = true
+							}
+						}
+						if !is_found {
+							points_to_traverse = append(points_to_traverse, test_coords)
+						}
+					}
+				}
+
+				if current_coords.Y > 1 {
+					test_coords := image.Point{current_coords.X, current_coords.Y - 1}
+					test_point := grid[test_coords]
+					if test_point == 0 {
+						peaks = append(peaks, test_coords)
+					}
+					if currentPoint-test_point == 1 || currentPoint-test_point == -1 {
+						is_found := false
+						for _, travelled_Point := range accessed_points {
+							if travelled_Point.X == test_coords.X && travelled_Point.Y == test_coords.Y {
+								is_found = true
+							}
+						}
+						for _, travelled_Point := range points_to_traverse {
+							if travelled_Point.X == test_coords.X && travelled_Point.Y == test_coords.Y {
+								is_found = true
+							}
+						}
+						if !is_found {
+							points_to_traverse = append(points_to_traverse, test_coords)
+						}
+					}
+				}
+
+				if current_coords.Y < y_max {
+					test_coords := image.Point{current_coords.X, current_coords.Y + 1}
+					test_point := grid[test_coords]
+					if test_point == 0 {
+						peaks = append(peaks, test_coords)
+					}
+					if currentPoint-test_point == 1 || currentPoint-test_point == -1 {
+						is_found := false
+						for _, travelled_Point := range accessed_points {
+							if travelled_Point.X == test_coords.X && travelled_Point.Y == test_coords.Y {
+								is_found = true
+							}
+						}
+						for _, travelled_Point := range points_to_traverse {
+							if travelled_Point.X == test_coords.X && travelled_Point.Y == test_coords.Y {
+								is_found = true
+							}
+						}
+						if !is_found {
+							points_to_traverse = append(points_to_traverse, test_coords)
+						}
+					}
+				}
+			}
+			fmt.Printf("%d\n", len(points_to_traverse))
+			fmt.Print(len("\n\n"))
+
+			points_to_traverse = points_to_traverse[1:]
+
+			accessed_points = append(accessed_points, current_coords)
+
+		}
+
+		ret = ret + len(peaks)
+	}
 	// solve part 1 here
-	return 42
+	return ret
 }
