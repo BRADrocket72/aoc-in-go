4c04a5db5465ce67f8318b8917c192bbbdd8f3e0
bradley <bradleyvaal@gmail.com>
2024-12-23 15:22:04 -0600

    move on to day 23

--- 2024/19/code.go
+++ 2024/19/code.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
 func main() {
 	aoc.Harness(run)
 }
+
+type Pattern struct {
+	characters       []string
+	towelsUsedToMake [][][]string
+}
+
+type Towel []string
 
 // on code change, run will be executed 4 times:
 // 1. with: false (part1), and example input
@@ -20,5 +31,32 @@ func run(part2 bool, input string) any {
 		return "not implemented"
 	}
 	// solve part 1 here
+	lines := strings.Split(input, "\n")
+	towelsLine := lines[0]
+	towelsStrings := strings.Split(towelsLine, ", ")
+	towels := make([]Towel, 0)
+	for _, towel := range towelsStrings {
+		towels = append(towels, strings.Split(towel, ""))
+	}
+
+	patterns := lines[2:]
+	for _, pattern := range patterns {
+		findAllCombos(towels, pattern)
+
+	}
 	return 42
+}
+
+func findAllCombos(towels []Towel, pattern string) {
+	for i, _ := range pattern {
+		runeValue, _ := utf8.DecodeRuneInString(pattern[i:])
+		for j, towel := range towels {
+			if i == 0 && j == 0 {
+				fmt.Print(towel)
+				fmt.Print(runeValue)
+
+			}
+		}
+	}
+
 }
---
+++ 2024/19/code_test.go
@@ -0,0 +1,9 @@
+package main
+
+func Test_towelPattern(pattern []string, towels [][]string) {
+	// bot := bot{position: image.Point{0, 0}, velocity: image.Point{1, 1}}
+	// if bot.position.X != 0 || bot.position.Y != 0 {
+	// 	t.Error("test error")
+	// }
+
+}
